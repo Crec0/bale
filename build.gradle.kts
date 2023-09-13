fe69092580ce3c194b921e698c04b2ed4b39b95d
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-09-12 23:31:19 -0700

    Remove Ktor and Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-@file:Suppress("PropertyName", "VulnerableLibrariesLocal")
-
-import com.google.common.io.Files
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 val archives_base_name: String by project
 val version: String by project
 val group: String by project
@@ -13,14 +8,15 @@ val minecraft_version: String by project
 val fabric_loader_version: String by project
 val fabric_api_version: String by project
 
-val ktor_version: String by project
-
 plugins {
-    kotlin("jvm")
-    kotlin("plugin.serialization").version("1.6.21")
-    id("fabric-loom")
+    val kotlinVersion = "1.9.10"
+
+    kotlin("jvm") version (kotlinVersion)
+    kotlin("plugin.serialization") version (kotlinVersion)
+
+    id("fabric-loom") version "1.3-SNAPSHOT"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.github.juuxel.loom-quiltflower") version "1.7.2"
+    id("io.github.juuxel.loom-quiltflower") version "1.8.0"
     java
 }
 
@@ -44,14 +40,6 @@ dependencies {
 
     modImplementation("net.fabricmc:fabric-language-kotlin:$fabric_kotlin_version")
     modImplementation("net.fabricmc.fabric-api:fabric-api:$fabric_api_version")
-
-    shadow("io.ktor:ktor-server-core-jvm:$ktor_version")
-    shadow("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    shadow("io.ktor:ktor-server-websockets-jvm:$ktor_version")
-    shadow("io.ktor:ktor-server-auth-jvm:$ktor_version")
-    shadow("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
-    shadow("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    shadow("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
 }
 
 tasks {
@@ -75,23 +63,13 @@ tasks {
         from("LICENSE")
     }
 
+    compileJava {
+        options.compilerArgs.add("--enable-preview")
+    }
+
     compileKotlin {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    withType<KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = "20"
         kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-    }
-
-    build {
-        doLast {
-            delete(shadowJar.get().archiveFile)
-            File("D:/minecraft_utils/server/mods/$archives_base_name-$version.jar").delete()
-            Files.copy(
-                File("./build/libs/$archives_base_name-$version.jar"),
-                File("D:/minecraft_utils/server/mods/$archives_base_name-$version.jar")
-            )
-        }
     }
 }
 
