781bdd54a9255368421ab2d09e41be1a26d37d42
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-09-12 23:31:56 -0700

    Add virtual threading

---
+++ src/main/java/dev/crec/bale/mixin/VirtualThreadFactoryMixin.java
@@ -0,0 +1,29 @@
+package dev.crec.bale.mixin;
+
+import net.minecraft.server.network.ServerConnectionListener;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.concurrent.ThreadFactory;
+
+@Mixin(ServerConnectionListener.class)
+public class VirtualThreadFactoryMixin {
+    @ModifyArg(
+            method = "method_14348",
+            at = @At(value = "INVOKE", target = "Lio/netty/channel/nio/NioEventLoopGroup;<init>(ILjava/util/concurrent/ThreadFactory;)V"),
+            index = 1
+    )
+    private static ThreadFactory factory(ThreadFactory factory) {
+        return Thread.ofVirtual().factory();
+    }
+
+    @ModifyArg(
+            method = "method_14349",
+            at = @At(value = "INVOKE", target = "Lio/netty/channel/epoll/EpollEventLoopGroup;<init>(ILjava/util/concurrent/ThreadFactory;)V"),
+            index = 1
+    )
+    private static ThreadFactory factory2(ThreadFactory factory) {
+        return Thread.ofVirtual().factory();
+    }
+}
