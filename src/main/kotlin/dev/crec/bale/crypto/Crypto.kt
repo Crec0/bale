c3e14c9313e0ba5ef97832c1d06498f6df337ccf
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-09-14 01:09:50 -0700

    Add cryptography stuff

---
+++ src/main/kotlin/dev/crec/bale/crypto/Crypto.kt
@@ -0,0 +1,75 @@
+package dev.crec.bale.crypto
+
+import java.nio.ByteBuffer
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+
+private const val ALGORITHM = "AES/GCM/NoPadding"
+private const val AES_KEY_BYTES = 256
+private const val TAG_LENGTH_BITS = 128
+private const val IV_LENGTH_BYTES = 128 / 8
+
+object Crypto {
+
+    fun genKey(): SecretKey {
+        val keyGen = KeyGenerator.getInstance("AES")
+        keyGen.init(AES_KEY_BYTES, SecureRandom.getInstanceStrong())
+
+        return keyGen.generateKey()
+    }
+
+    private fun genNonce(): ByteArray {
+        val nonce = ByteArray(IV_LENGTH_BYTES)
+        SecureRandom().nextBytes(nonce)
+
+        return nonce
+    }
+
+    fun encrypt(text: String, key: SecretKey): ByteArray {
+        val iv = genNonce()
+
+        val encryptedBytes = Cipher.getInstance(ALGORITHM).run {
+            init(Cipher.ENCRYPT_MODE, key, GCMParameterSpec(TAG_LENGTH_BITS, iv))
+            doFinal(text.encodeToByteArray())
+        }
+
+        return ByteBuffer.allocate(iv.size + encryptedBytes.size).run {
+            put(iv)
+            put(encryptedBytes)
+            array()
+        }
+    }
+
+    fun decrypt(encrypted: ByteArray, key: SecretKey): ByteArray {
+        val buffer = ByteBuffer.wrap(encrypted)
+        val iv = ByteArray(IV_LENGTH_BYTES).apply { buffer.get(this) }
+        val textBytes = ByteArray(buffer.remaining()).apply { buffer.get(this) }
+
+        val decryptedBytes = Cipher.getInstance(ALGORITHM).run {
+            val gcmSpec = GCMParameterSpec(TAG_LENGTH_BITS, iv)
+            init(Cipher.DECRYPT_MODE, key, gcmSpec)
+
+            doFinal(textBytes)
+        }
+
+        return decryptedBytes
+    }
+
+    @OptIn(ExperimentalEncodingApi::class)
+    fun keyFromBase64(b64: String): SecretKey {
+        return SecretKeySpec(Base64.decode(b64), "AES")
+    }
+
+    @OptIn(ExperimentalEncodingApi::class)
+    fun SecretKey.base64() = Base64.encode(this.encoded)
+
+    @OptIn(ExperimentalEncodingApi::class)
+    fun ByteArray.base64() = Base64.encode(this)
+}
